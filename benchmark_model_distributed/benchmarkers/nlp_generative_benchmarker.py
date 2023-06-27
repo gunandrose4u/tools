80040d191d803e338b412dbf3bb01c38e36e885c
Joe Zhu <jozh@microsoft.com>
2023-06-27 11:53:20 +0800

    update

--- benchmark_model_distributed/benchmarkers/nlp_generative_benchmarker.py
+++ benchmark_model_distributed/benchmarkers/nlp_generative_benchmarker.py
@@ -25,18 +25,19 @@ class NlpGenerativeBenchmarker(DirectBenchmarker):
             num_new_tokens = len(t_times)
             if num_new_tokens > 0:
                 prompt_phase_predict_times.append(t_times[0])
-            
+
             if num_new_tokens > 1:
                 token_phase_predict_times_from_token_recoder.extend(t_times[1:])
 
         if self._run_config.token_metrics:
             self._get_percentile_metrics(res_benchmark, prompt_phase_predict_times, 'prompt_phase_latency_')
-            
+
             token_phase_predict_times = []
-            for i in range(len(query_predict_times)):
-                token_phase_predict_times.append(query_predict_times[i] - prompt_phase_predict_times[i])
+            if len(query_predict_times) == len(prompt_phase_predict_times):
+                for i in range(len(query_predict_times)):
+                    token_phase_predict_times.append(query_predict_times[i] - prompt_phase_predict_times[i])
             self._get_percentile_metrics(res_benchmark, token_phase_predict_times, 'token_phase_latency_')
-            
+
             if not self._run_config.verbose:
                 logger.info(f"query_predict_times\n{query_predict_times}")
                 logger.info(f"prompt_phase_predict_times\n{prompt_phase_predict_times}")
@@ -47,8 +48,8 @@ class NlpGenerativeBenchmarker(DirectBenchmarker):
             if token_phase_predict_times_from_token_recoder:
                 self._get_percentile_metrics(res_benchmark, token_phase_predict_times_from_token_recoder, 'r_token_phase_latency_')
                 res_benchmark["r_token/s"] = 1 / np.mean(token_phase_predict_times_from_token_recoder)
-            
+
             res_benchmark["token/s"] = (self._run_config.max_new_tokens - 1) / np.mean(token_phase_predict_times)
 
-            
+
         return res_benchmark
